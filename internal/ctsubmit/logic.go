b9efd821d6244a2067f28295f913aec2f6e2edc0
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2024-06-29 17:56:22 +0530

    EKU changes

--- internal/ctsubmit/logic.go
+++ internal/ctsubmit/logic.go
@@ -87,10 +87,21 @@ func (d *stageZeroData) stageZero(ctx context.Context, reqBody io.ReadCloser, pr
 		return nil, http.StatusBadRequest, fmt.Errorf("chain is empty")
 	}
 
-	chain, err := ctfe.ValidateChain(req.Chain,
-		ctfe.NewCertValidationOpts(d.roots, time.Time{},
-			false, false, &d.notAfterStart, &d.notAfterLimit,
-			false, []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}))
+	// print the EKU of the first certificate in the chain
+	certalkf, _ := x509.ParseCertificate(req.Chain[0])
+	log.Printf("EKU of the first certificate in the chain: %v", certalkf.ExtKeyUsage)
+
+	// TODO: What EKU parameters should be accepted by the log?
+	// The trillian integration tests include leaf certificates without any EKU parameters
+
+	// validationOpts := ctfe.NewCertValidationOpts(d.roots, time.Time{},
+	// 	false, false, &d.notAfterStart, &d.notAfterLimit,
+	// 	false, []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth})
+	validationOpts := ctfe.NewCertValidationOpts(d.roots, time.Time{},
+		false, false, &d.notAfterStart, &d.notAfterLimit,
+		false, nil)
+
+	chain, err := ctfe.ValidateChain(req.Chain, validationOpts)
 	if err != nil {
 		return nil, http.StatusBadRequest, fmt.Errorf("unable to validate chain: %w", err)
 	}
