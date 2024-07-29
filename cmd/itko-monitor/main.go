869d487ca725aa685a0fd2178d3c5ea41f692149
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2024-07-29 10:39:45 -0700

    Add build step to integration

--- cmd/itko-monitor/main.go
+++ cmd/itko-monitor/main.go
@@ -12,13 +12,14 @@ import (
 
 func main() {
 	// Parse the command-line flags
+	storeDirectory := flag.String("store-directory", "", "Tile storage directory. Must not have a trailing slash.")
 	storeAddress := flag.String("store-address", "", "Tile storage url. Must end with a trailing slash.")
 	listenAddress := flag.String("listen-address", "", "IP and port to listen on for incoming connections.")
 	maskSize := flag.Int("mask-size", 0, "Mask size for the quadtree.")
 	flag.Parse()
 
-	if *storeAddress == "" {
-		fmt.Println("Error: -store-address flag must be set")
+	if *storeDirectory == "" || *storeAddress == "" {
+		fmt.Println("Error: -store-address or -store-address flag must be set")
 		flag.Usage() // Print the usage message
 		os.Exit(1)   // Exit with a non-zero status
 	}
@@ -40,5 +41,5 @@ func main() {
 		log.Fatalf("failed to bind to address: %v", err)
 	}
 
-	ctmonitor.MainMain(listener, *storeAddress, *maskSize, nil)
+	ctmonitor.MainMain(listener, *storeDirectory, *storeAddress, *maskSize, nil)
 }
