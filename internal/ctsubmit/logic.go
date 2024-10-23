c44eaf37ad80e8a7856426b6c1631af529c18cfb
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2024-10-23 14:29:30 -0400

    remove a bit of excessive logging

--- internal/ctsubmit/logic.go
+++ internal/ctsubmit/logic.go
@@ -107,13 +107,7 @@ func (d *stageZeroData) stageZero(ctx context.Context, reqBody io.ReadCloser, pr
 
 	chain, err := ctfe.ValidateChain(req.Chain, validationOpts)
 	if err != nil {
-		var base64Array []string
-		for _, item := range req.Chain {
-			encoded := base64.StdEncoding.EncodeToString(item)
-			base64Array = append(base64Array, encoded)
-		}
-
-		return nil, http.StatusBadRequest, fmt.Errorf("unable to validate chain: %w. Chain: %+v", err, base64Array)
+		return nil, http.StatusBadRequest, fmt.Errorf("unable to validate chain: %w", err)
 	}
 
 	isPrecert, err := ctfe.IsPrecertificate(chain[0])
